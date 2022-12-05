a055a431e3078ac76e1b61d26f86b2c546ad6da3
Hyunsik Choi <gomgom_@naver.com>
2022-12-05 23:31:26 +0900

    Update # 알고리즘 7강 분할과 정복.js

--- # 알고리즘 7강 분할과 정복.js
+++ # 알고리즘 7강 분할과 정복.js
@@ -9,11 +9,27 @@ function search(arr, num) {
   return -1;
 }
 
-console.log(search([1,2,3,4,5,6], 4)) // 5, 
-// 배열이 수만개가 있다고 한다면 앞에서부터 하나씩 선형탐색 하면 오래걸리니 
-// 중간지점부터 탐색한다 = 이진 탐색
-// 이진 탐색 Binary Search
+console.log(search([1, 2, 3, 4, 5, 6], 4)); 
 
-function binarySearch(arr, num) {
-  
+// 배열이 수만개가 있다고 한다면 앞에서부터 하나씩 선형탐색 하면 오래걸리니
+// 중간지점부터 탐색한다 = 이진 탐색
+// 이진 탐색 Binary Search; O(log n)
+function binarySearch(arr, val) {
+  let min = 0; // 0
+  let max = arr.length - 1; // 13
+
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2); // 6
+    if (arr[mid] < val) {
+      min = mid + 1;
+    } else if (arr[mid] > val) {
+      max = mid - 1;
+    } else {
+      return mid
+    }
+  }
+  // 없으면
+  return -1;
 }
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], 14));
