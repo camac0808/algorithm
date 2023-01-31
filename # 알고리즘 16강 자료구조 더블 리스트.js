0554a75091f8ad870ed717a802bb83ee8087c5ed
Hyunsik Choi <gomgom_@naver.com>
2023-01-31 19:04:46 +0900

    DLL 마무리

--- # 알고리즘 16강 자료구조 더블 리스트.js
+++ # 알고리즘 16강 자료구조 더블 리스트.js
@@ -5,7 +5,7 @@ class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
-    this.prev = null;  
+    this.prev = null;
   }
 }
 
@@ -21,9 +21,137 @@ class DoubleLinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev; // tail을 이전 값으로 바꿔준다
+      this.tail.next = null; // next 연결을 끊어준다
+      poppedNode.prev = null; // 연결이 두개가 있으므로 prev 포인터도 끊어줘야 된다
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    // length === 0 이면 return undefined
+    // length === 1 이면 head tail = null
+    // this.head를 next값으로 바꾼다
+    // 기존 head.next = null
+    // head.next.prev = null
+    // length--;
+    // return old head
+    if (this.length === 0) return undefined;
+    let oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0
+    let current = this.head
+    let mid = Math.floor(this.length / 2);
+
+    if (index > mid) {
+      current = this.tail;
+      counter = this.length - 1;
+      while (counter !== index) {
+        current = current.prev;
+        counter--;
+      }
+      return current;
+    } 
+    else if (index <= mid) {
+      counter = 0;
+      current = this.head;
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    }
+  }
+  set(index, val) {
+    if (index < 0 || index >= this.length) return false;
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert (index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+
+    let newNode = new Node(val);
+    let beforeNode = this.get(index - 1);
+    let afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+  remove (index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let removedNode = this.get(index);
+    let beforeNode = removedNode.prev;
+    let afterNode = removedNode.next;
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length--;
+    return true;
   }
 }
 
+let list = new DoubleLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.remove(0);
 
+
+console.log(list);
