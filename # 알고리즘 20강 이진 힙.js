c4cf3e613b683b5ed72c5acd93f8857627473b4d
Hyunsik Choi <gomgom_@naver.com>
2023-02-28 08:00:06 +0900

    우선순위 큐

--- # 알고리즘 20강 이진 힙.js
+++ # 알고리즘 20강 이진 힙.js
@@ -1,36 +1,88 @@
 // Binary Heap(더미)
 // Max Binary Heap 부모가 자식보다 무조건 크다 (왼쪽 오른쪽 순서 상관없이)
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+// Min Binary Heap 자식이 부모보다 무조건 크다
+
 
 class BinaryHeap {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [99, 20, 15, 30, 40, 60];
   }
   insert(value) {
     this.values.push(value);
-    this.bubbleUp();
+    var insertIndex = this.values.length - 1;
+    var element = this.values[insertIndex];
+
+    while (true) {
+      var parentIndex = Math.floor((insertIndex - 1) / 2);
+      if (this.values[parentIndex] < value) {
+        this.values[insertIndex] = this.values[parentIndex];
+        this.values[parentIndex] = element;
+        insertIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
   }
-  bubbleUp() {
-    let index = this.values.length - 1;
-    const element = this.values[index];
-    
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.values[parentIndex];
-      if (element <= parent) break;
-      this.values[parentIndex] = element;
-      this.values[index] = parent;
-      index = parentIndex;
+
+  extractMax() {
+    // root 제거 배열의 첫번째 값
+    // 배열의 마지막 값 root로 대입
+    // 자식값 중 큰 값과 비교해서 작으면 swap 해주기
+    // 크면 그대로 냅둠
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    let index = 0;
+    let element = this.values[index];
+    let largerSiblingIndex = 0;
+
+    // swap
+    while (true) {
+      // element의 자식
+      let leftChildIndex = index * 2 + 1;
+      let rightChildIndex = index * 2 + 2;
+      let leftChild = this.values[leftChildIndex];
+      let rightChild = this.values[rightChildIndex];
+      console.log(this.values, leftChildIndex, this.values.length);
+      
+      if (leftChildIndex < this.values.length) {
+        if (rightChild === undefined) {
+          rightChild = 0;
+        }
+        leftChild > rightChild ? largerSiblingIndex = leftChildIndex : largerSiblingIndex = rightChildIndex;
+        if (element > this.values[largerSiblingIndex]) break;
+        this.values[index] = this.values[largerSiblingIndex];
+        this.values[largerSiblingIndex] = element;
+        index = largerSiblingIndex;
+      } else {
+        break;
+      }
     }
   }
 }
 
 let heap = new BinaryHeap();
-heap.insert(1)
-console.log(heap.values)
+heap.insert(80);
+console.log(heap.values);
+heap.extractMax();
+heap.extractMax();
+console.log(heap.values);
+
+// 우선순위 큐 Priority Queue
+// Node : value, priority
+
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  insert(value, priority) {
+    this.values.push(new Node(value, priority));
+  }
+}
